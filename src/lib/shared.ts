027aeb879366414f65cd9bd351be22c949801ab1
jwood <jwood@nba.com>
2025-07-12 16:31:28 -0400

    Input quantity calculator

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,3 +1,5 @@
+import {IndustryActivityMaterial} from "@/lib/db/collections";
+
 export type eveImageTypes = {
     category: 'alliances' | 'corporations' | 'characters' | 'types';
     id: string | number;
@@ -12,10 +14,46 @@ export type eveImageTypes = {
  * @param variation
  * @param size
  */
-export function eveImageUrl( { category, id, variation = 'icon', size = 32 }: eveImageTypes ) {
+export function eveImageUrl({category, id, variation = 'icon', size = 32}: eveImageTypes) {
     const baseUrl = new URL('https://images.evetech.net/');
     baseUrl.pathname = `/${category}/${id}/${variation}`;
-    baseUrl.searchParams.set( 'size', size?.toString() );
+    baseUrl.searchParams.set('size', size?.toString());
 
     return baseUrl.toString();
+}
+
+export interface MaterialCostInput {
+    baseQty: number;         // Base material requirement from the BPO
+    bpME: number;           // Blueprint Material Efficiency (e.g., 0.10 for 10%)
+    structureME: number;     // Structure Material Efficiency (e.g., 0.025 for 2.5%)
+}
+
+/**
+ * Calculates the material input quantity taking into account bonuses.
+ * @param baseQty
+ * @param bpoME
+ * @param structureME
+ */
+export function calcInputQty( {
+        baseQty,
+        bpME,
+        structureME,
+    }: MaterialCostInput ): number
+{
+    return Math.ceil(
+        baseQty * (1 - bpME) * (1 - structureME)
+    );
+}
+
+/**
+ * Calculates a whole list of IndustryActivityMaterial documents.
+ * @param materials
+ * @param bpME
+ * @param structureME
+ */
+export function calcIndustryActivityMaterials( materials: IndustryActivityMaterial[], bpME: number, structureME: number ): IndustryActivityMaterial[] {
+    return materials.map( material => { return {
+        ...material,
+        quantity: calcInputQty({baseQty: material.quantity, bpME, structureME} )
+    } } );
 }
