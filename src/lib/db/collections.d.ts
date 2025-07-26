1f7c7073651407980256f99f3d324795ae715605
JayWood <jjwood2004@gmail.com>
2025-07-25 23:36:45 -0400

    Allow titles

--- src/lib/db/collections.d.ts
+++ src/lib/db/collections.d.ts
@@ -209,6 +209,7 @@ type StructureInfo = {
 }
 
 type PriceComparison = {
+    title: string;
     typeIds: number[];
     sourceStructure: StructureInfo["structureId"]
     targetStructure: StructureInfo["structureId"]
